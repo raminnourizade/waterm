78414676f34929ff80a61d75261b7e930b318dc4
Ramin Nourizade <ramin@email.com>
2025-07-16 17:38:02 +0330

    add logo

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -32,42 +32,48 @@ class _LoginPageState extends State<LoginPage> {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('ورود به برنامه'),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
+        backgroundColor: Color(0xFFE0F7FA), // آبی روشن
+        body: Center(
           child: Padding(
             padding: const EdgeInsets.all(24.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 60),
+                Image.asset('assets/logo.png', height: 100),
+                const SizedBox(height: 16),
                 const Text(
-                  'به برنامه خوش آمدید!',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  '  سامانه شرکت آب و فاضلاب',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFF01579B)),
                 ),
                 const SizedBox(height: 32),
                 TextField(
                   controller: usernameController,
                   decoration: const InputDecoration(
                     labelText: 'نام کاربری',
+                    filled: true,
+                    fillColor: Colors.white,
                     border: OutlineInputBorder(),
                   ),
                 ),
                 const SizedBox(height: 16),
                 TextField(
                   controller: passwordController,
+                  obscureText: true,
                   decoration: const InputDecoration(
                     labelText: 'رمز عبور',
+                    filled: true,
+                    fillColor: Colors.white,
                     border: OutlineInputBorder(),
                   ),
-                  obscureText: true,
                 ),
                 const SizedBox(height: 24),
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0288D1), // آبی اصلی
+                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 14),
+                  ),
                   onPressed: _login,
-                  child: const Text('ورود'),
+                  child: const Text('ورود', style: TextStyle(fontSize: 18)),
                 ),
               ],
             ),
@@ -75,5 +81,6 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+
   }
 }
